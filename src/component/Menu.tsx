c69afb73953d5af4f1786e57d8e8eb0167750119
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2026-05-01 09:51:39 +0200

    Fixed the dropdown menu display bug

--- src/component/Menu.tsx
+++ src/component/Menu.tsx
@@ -1,10 +1,91 @@
+import { useState, useEffect, useRef } from "react";
+
 import { Link } from "react-router-dom";
 
+import '../style/Menu.css'
 
 function Menu() {
+    const [image, Setimage] = useState("../images/images/menu-burger.png");
+    const [value, setValue] = useState(0);
+    const [height, setHeight] = useState(0);
+    const containerRef = useRef<HTMLUListElement | null>(null);
+    const [windowWidth,setWindowWidth] = useState<boolean>(window.matchMedia('(min-width: 801px)').matches);
+    const measureHeight = () => {
+        if (containerRef.current) {
+          setHeight(containerRef.current.scrollHeight);
+        }
+      };
+    const menu = containerRef.current;
+    useEffect(() => {
+      window.addEventListener("resize", () => {
+      const menu = containerRef.current;
+
+      if (!menu) return;
+
+      setWindowWidth(window.matchMedia("(min-width: 801px)").matches);
+      if (window.matchMedia("(min-width: 801px)").matches) {
+        menu.style.visibility = "visible";
+        menu.style.height = `${height}px`;
+      } else {
+      if (value === 1) {
+        menu.style.visibility = "visible";
+        menu.style.marginTop='80px';
+      } else {
+        menu.style.marginTop=`-${height+80}px`;
+        menu.style.visibility = "hidden";
+    }
+    }
+ }); 
+ }, [windowWidth, height, value]);
+    
+      useEffect(() => {
+        measureHeight();
+      }, []);
+
+    const openMenu = () => {
+  
+    if(menu){
+  
+    if (value === 0) {
+      setValue(1);
+      Setimage("../images/images/menu-burger1.png");
+      menu.style.marginTop='80px';
+      menu.style.visibility = "visible";
+      // transform: scale(1);
+      menu.style.height = `${height}px`;
+    } else {
+      setValue(0);
+      Setimage("../images/images/menu-burger.png");
+      menu.style.marginTop=`-${height+80}px`;
+      // menu.style.height = "0px";
+      setTimeout(() => {
+        menu.style.visibility = "hidden";
+      }, 250); // Durée de l'animation de fermeture
+    }}
+  };
+
   return (
+    <header className='vertical-menu'>
+      <div>
+        <p>
+          Z
+        </p>
+        <p>
+          ZENVYA<br />
+          Assistant intelligent
+        </p>
+      </div>
     <nav className="menu">
-      <ul>
+      <button type="button" onClick={openMenu}>
+        <img src={image} alt="Ouverture du menu" />
+      </button>
+                <ul
+            className="open-menu"
+            ref={containerRef}
+            style={{
+              transition: "margin-top 0.3s ease",
+            }}
+            >
         <li><Link to="dashboard">Dashboard</Link></li>
         <li><Link to="health">Santé</Link></li>
         <li><Link to="finance">Finances</Link></li>
@@ -13,7 +94,7 @@ function Menu() {
         <li><Link to="ai-consulting">AI conseils</Link></li>
       </ul>
     </nav>
-
+  </header>
   );
 }
 export default Menu;
